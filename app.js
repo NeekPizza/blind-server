ba6adc6d97e69ef4377560c923ecf12956624012
Nick Piazza <nick@onlinemeded.org>
2021-02-17 19:19:04 -0600

    fix cors error

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const http = require('http');
-const socketIo = require("socket.io");
 
 const port = process.env.PORT || 4001;
 const index = require('./routes/index');
@@ -10,10 +9,17 @@ app.use(index);
 
 const server = http.createServer(app);
 
-const io = socketIo(server);
+const io = require("socket.io")(server, {
+    cors: {
+      origin: '*',
+    }
+  });
 
 io.on("connection", (socket) => {
-    socket.emit("hello world");
+    socket.emit('hello', 'world');
+    socket.on('disconnect', () => {
+        console.log('Client disconnected');
+      });
 });
 
 server.listen(port, () => console.log(`Listening on port ${port}`));
