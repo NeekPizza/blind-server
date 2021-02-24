8edc16d741159caec7928fde03d55593772b4944
Nick Piazza <nick@onlinemeded.org>
2021-02-23 21:29:19 -0600

    implement testing framework

---
+++ test/test.js
@@ -0,0 +1,34 @@
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+const Client = require("socket.io-client");
+const assert = require("chai").assert;
+
+describe("tests", () => {
+  let io, serverSocket, clientSocket;
+
+  before((done) => {
+    const httpServer = createServer();
+    io = new Server(httpServer);
+    httpServer.listen(() => {
+      const port = httpServer.address().port;
+      clientSocket = new Client(`http://localhost:${port}`);
+      io.on("connection", (socket) => {
+        serverSocket = socket;
+      });
+      clientSocket.on("connect", done);
+    });
+  });
+
+  after(() => {
+    io.close();
+    clientSocket.close();
+  });
+
+  it("should work", (done) => {
+    clientSocket.on("hello", (arg) => {
+      assert.equal(arg, "world");
+      done();
+    });
+    serverSocket.emit("hello", "world");
+  });
+});
